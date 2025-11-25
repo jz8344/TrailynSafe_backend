e7b5a0cc63e295aa0951d4e5c1288d6371967e7d
jz8344 <jose.zanabria.23e@utzmg.edu.mx>
2025-11-24 19:31:00 -0600

    V2.5

--- app/Models/Escuela.php
+++ app/Models/Escuela.php
@@ -4,10 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Attributes\ObservedBy;
-use App\Observers\EscuelaObserver;
 
-#[ObservedBy([EscuelaObserver::class])]
 class Escuela extends Model
 {
     use HasFactory;
