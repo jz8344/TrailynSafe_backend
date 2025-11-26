129a042f433cd57a8ca3db2879a86a6df5314fbe
jz8344 <jose.zanabria.23e@utzmg.edu.mx>
2025-11-25 20:57:23 -0600

    fix: permitir iniciar rutas con estado 'pendiente' o 'activa'

--- app/Models/Ruta.php
+++ app/Models/Ruta.php
@@ -79,8 +79,8 @@ class Ruta extends Model
 
     public function iniciar()
     {
-        if ($this->estado !== 'activa') {
-            throw new \Exception('Solo se pueden iniciar rutas activas');
+        if (!in_array($this->estado, ['pendiente', 'activa'])) {
+            throw new \Exception('Solo se pueden iniciar rutas pendientes o activas');
         }
         
         $this->estado = 'en_progreso';
